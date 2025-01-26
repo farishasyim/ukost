f74806736a0af27dd997653810ab72a732b9a87d
farishasyim <faris.hasyim.03@gmail.com>
2025-01-26 19:09:30 +0700

    minor update

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -1,5 +1,6 @@
 class Routes {
   static const String development = "http://192.168.100.14:8000";
+  static const String production = "https://ukost.farishasyim.my.id";
 
   static const String endpoint = development;
 
